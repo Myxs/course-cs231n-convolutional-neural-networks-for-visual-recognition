5cee7236745c8aaeb911c9f70ce272040b378e28
Zeyuan Shang <zeyuanxy@gmail.com>
2015-12-10 16:42:45 -0500

    softmax_loss_vectorized is finished

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -64,7 +64,20 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_train = X.shape[1]
+
+  scores = W.dot(X)
+  scores -= np.max(scores, axis = 0)
+  correct_scores = scores[y, range(len(y))]
+  scores = np.exp(scores)
+  loss = np.sum(-correct_scores + np.log(np.sum(scores, axis = 0)))
+  loss /= num_train
+  loss += 0.5 * reg * np.sum(W * W)
+
+  scores /= np.sum(scores, axis = 0)
+  scores[y, range(len(y))] -= 1
+  dW = scores.dot(X.T)
+  dW /= num_train
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
