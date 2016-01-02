491f38ad70fead2cb98cfc103d4f5b6c39fdf8aa
Zeyuan Shang <zeyuanxy@gmail.com>
2016-01-02 11:27:52 +0800

    forward and backward of Affine layer is finished

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -22,7 +22,7 @@ def affine_forward(x, w, b):
   # TODO: Implement the affine forward pass. Store the result in out. You     #
   # will need to reshape the input into rows.                                 #
   #############################################################################
-  pass
+  out = x.reshape(x.shape[0], -1).dot(w) + b
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -50,7 +50,10 @@ def affine_backward(dout, cache):
   #############################################################################
   # TODO: Implement the affine backward pass.                                 #
   #############################################################################
-  pass
+  xT = x.reshape(x.shape[0], -1).T
+  dx = dout.dot(w.T).reshape(x.shape)
+  dw = xT.dot(dout)
+  db = np.sum(dout, axis = 0)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
